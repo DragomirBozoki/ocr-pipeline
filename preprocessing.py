b6d0405725de27816036fd9e80ae19a1c1f1e485
Dragomir Bozoki <82651750+DragomirBozoki@users.noreply.github.com>
2025-07-13 14:36:00 +0200

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -5,17 +5,17 @@ import numpy as np
 AMOUNT_REGION = (110, 180, 880, 330) # x1, y1, x2, y2
 
 def preprocess_frame(frame):
-    # 1. Cropuj regiju interesa
+
     x1, y1, x2, y2 = AMOUNT_REGION
     roi = frame[y1:y2, x1:x2]
 
     # 2. U sivo
     gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
 
-    # 3. Blur za uklanjanje šuma
+  
     blurred = cv2.GaussianBlur(gray, (3, 3), 0)
 
-    # 4. Adaptive Threshold
+
     thresh = cv2.adaptiveThreshold(
         blurred, 255,
         cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
@@ -23,7 +23,7 @@ def preprocess_frame(frame):
         11, 2
     )
 
-    # 5. Dilation da spoji tačkice u ciframa/slovima
+
     kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (2, 2))
     dilated = cv2.dilate(thresh, kernel, iterations=1)
 
